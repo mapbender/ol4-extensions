4b242515dbc671e11bd8e7d191af05af06e36775
Felix Rauch <felix.rauch@wheregroup.com>
2019-08-23 13:40:56 +0200

    overwrite Method

--- geoJSONWithSeperateData.js
+++ geoJSONWithSeperateData.js
@@ -7,6 +7,47 @@
 
     ol.inherits(ol.format.GeoJSONWithSeperateData, ol.format.GeoJSON);
 
+
+    /**
+     * Method differs from its parent's method by only delivering features with valid geometry
+     * @param object
+     * @param opt_options
+     * @returns {Array<ol.Feature>}
+     */
+    ol.format.GeoJSONWithSeperateData.prototype.readFeaturesFromObject = function(
+        object, opt_options) {
+        var geoJSONObject = /** @type {GeoJSONObject} */ (object);
+        /** @type {Array.<ol.Feature>} */
+        var features = null;
+        if (geoJSONObject.type === 'FeatureCollection') {
+            var geoJSONFeatureCollection = /** @type {GeoJSONFeatureCollection} */
+                (object);
+            features = [];
+            var geoJSONFeatures = geoJSONFeatureCollection.features;
+            var i, ii;
+            for (i = 0, ii = geoJSONFeatures.length; i < ii; ++i) {
+                var feature = this.readFeatureFromObject(geoJSONFeatures[i],
+                    opt_options);
+                if (feature.getGeometry()) {
+                    features.push();
+                } else {
+                    console.warn("Feature "+feature.getId()+" does not have a valid geometry and is therefore ommited");
+                }
+            }
+        } else {
+            features = [this.readFeatureFromObject(object, opt_options)];
+        }
+        return features;
+    };
+
+    /**
+     * Method distinguishes from its parent's method by not storing the GeoJsonFeature's properties in the Feature's properties but wraps it in a data property within the Feature's properties
+     *
+     * @param object
+     * @param opt_options
+     * @returns {ol.Feature}
+     */
+
     ol.format.GeoJSONWithSeperateData.prototype.readFeatureFromObject = function (object, opt_options) {
         /**
          * @type {GeoJSONFeature}
